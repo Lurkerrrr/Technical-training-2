cb4e3758f51705c35fa3f12160b8997562b462dc
debugger <meganesh2005@gmail.com>
2025-04-01 20:50:04 +0200

    init

---
+++ projekt/ProjectCinema.domain/Repositories/IMovieRepository.cs
@@ -0,0 +1,23 @@
+﻿using ProjectCinema.domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCinema.domain.Repositories
+{
+    public interface IMovieRepository
+    {
+        Movie GetById(long id);
+        IEnumerable<Movie> GetAll();
+        IEnumerable<Category> GetCategories();
+        bool IsMovieExist(string name, int year);
+        bool IsSeanceExist(DateTime seanceDate);
+        void Add(Movie movie);
+        void Update(Movie movie);
+        Movie GetSeanceDetails(long movieId);
+        List<Seance> GetSeanceByMovieId(long movieId);
+        void Remove(Movie movie);
+    }
+}
---
+++ projekt/ProjectCinema.infrastructure/Repository/MoviesRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectCinema.domain.Entities;
+using ProjectCinema.domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCinema.infrastructure.Repository
+{
+    public class MoviesRepository : IMovieRepository
+    {
+        private readonly CinemaTicketDbContext _context;
+
+        public MoviesRepository(CinemaTicketDbContext context) 
+        { 
+            _context = context;
+        }
+
+        public Movie GetById(long id) 
+        {
+            return _context.Movies.Include(c => c.Seances).ThenInclude(c => c.Tickets).SingleOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Movie> GetAll() 
+        { 
+            return _context.Movies.ToList();
+        }
+
+        public bool IsMovieExist(string name, int year) 
+        {
+            return _context.Movies.Any(x => x.Name == name && x.Year == year);
+        }
+
+        public bool IsSeanceExist(DateTime seanceDate) 
+        {
+            return _context.Seances.Any(x => x.Date == seanceDate);
+        }
+
+        public List<Seance> GetSeancesByMovieId(long movieId) 
+        { 
+            return _context.Seances.Where(x => x.MovieId == movieId).ToList();
+        }
+    }
+}
